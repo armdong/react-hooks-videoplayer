80ccf2df9f496e904cc20c21fc61402002579c23
wangmaodong <3003835782@qq.com>
2019-07-20 12:35:20 +0800

    Updates

---
+++ precache-manifest.ffa5831c83be733dc1f50905523a6c29.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b96325b75be6098e2b20aa7d1ba64e53",
+    "url": "/react_videoplayer/index.html"
+  },
+  {
+    "revision": "9435f64085af3e177362",
+    "url": "/react_videoplayer/static/js/2.b8e77952.chunk.js"
+  },
+  {
+    "revision": "1e645c06314a69fa3d53",
+    "url": "/react_videoplayer/static/js/main.6f1a433d.chunk.js"
+  },
+  {
+    "revision": "9d808bd6d5131ace177f",
+    "url": "/react_videoplayer/static/js/runtime~main.08c55151.js"
+  }
+]);
